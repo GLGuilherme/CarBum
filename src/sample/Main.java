4ccba8d432828335e36e008164cca5ba6641840a
GLGuilherme <gabrielopes31@hotmail.com>
2018-08-28 11:32:47 -0300

    Tela 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.swing.text.html.ImageView;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("CarBum");
+        primaryStage.setScene(new Scene(root, 1100, 700));
         primaryStage.show();
     }
 
