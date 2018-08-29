a23fbdf7589e6c795eb633fbee966e1bf65489d4
GLGuilherme <gabrielopes31@hotmail.com>
2018-08-29 12:25:24 -0300

    aa

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,17 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
-import javax.swing.text.html.ImageView;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Tela1.fxml"));
         primaryStage.setTitle("CarBum");
-        primaryStage.setScene(new Scene(root, 1100, 700));
+        primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
     }
 
