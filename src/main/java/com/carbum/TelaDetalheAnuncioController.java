cbaf74790e241a10a8c2e1713818058862112a96
Cassiano Montanari <cassianomontanari@gmail.com>
2018-10-25 23:02:33 -0300

    Criado tela de detalhamento do anuncio

--- src/main/java/com/carbum/TelaDetalheAnuncioController.java
+++ src/main/java/com/carbum/TelaDetalheAnuncioController.java
@@ -1,10 +1,30 @@
 package com.carbum;
 
 import com.carbum.anuncio.DAOAnuncio;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class TelaDetalheAnuncioController {
     public static String idAnuncioClicado;
 
+    @FXML
+    private AnchorPane rootPane;
 
 
+    @FXML
+    public void navegaTelaAnterior() throws IOException {
+        rootPane.getChildren().clear();
+
+        try {
+            AnchorPane telaPecaPesquisas = (AnchorPane) FXMLLoader.load(getClass()
+                    .getResource("/fxml/TelaInicial.fxml"));
+
+            rootPane.getChildren().setAll(telaPecaPesquisas);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
